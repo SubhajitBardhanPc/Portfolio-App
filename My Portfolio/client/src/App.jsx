929d16a124fc5f378a28999c1e45d3af5ea5f393
bardhansubhajit3@gmail.com <bardhansubhajit3@gmail.com>
2025-04-16 17:54:32 +0530

    About Page Done

--- My Portfolio/client/src/App.jsx
+++ My Portfolio/client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         
       </Routes>
     </Router>
---
+++ My Portfolio/client/src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="about-content">
+            <h1>About Me</h1>
+            <p>
+                Welcome to my portfolio! I am a passionate developer with a love for creating
+                innovative and efficient solutions. My journey into the world of technology began
+                with a curiosity for problem-solving, which has transformed into a thriving career
+                in software development.
+            </p>
+            <p>
+                I specialize in a variety of technologies, ranging from front-end development using 
+                frameworks like React to back-end programming with Node.js. I constantly strive to 
+                learn new skills and enhance my expertise to keep up with the ever-evolving tech landscape.
+            </p>
+            <p>
+                When I am not coding, you can find me exploring new places, indulging in photography, 
+                or experimenting with creative hobbies that keep my mind inspired.
+            </p>
+        </div>
+    );
+};
+
+export default About;

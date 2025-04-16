406b51a4bbb6ca191dddadea904524502fadf01f
bardhansubhajit3@gmail.com <bardhansubhajit3@gmail.com>
2025-04-16 18:53:02 +0530

    Done

--- My Portfolio/client/src/components/Navbar.jsx
+++ My Portfolio/client/src/components/Navbar.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom"; // Importing Link from react-router-dom
 import "./Navbar.css"; // Importing CSS file
 import logo from "../assets/logo.png"; // Importing logo image
-const Navbar = () => {
-    
 
+const Navbar = () => {
     return (
         <header className="header">
             <div className="logo">
-              {/* Add the developer logo here */}
-              <img src={logo}  className="developer-logo" />
-                
+                <img src={logo} alt="Developer Logo" className="developer-logo" />
             </div>
-           
+
             <nav className="navbar">
-                <a href="/" className="nav-link">Home</a>
-                <a href="/about" className="nav-link">About</a>
-                <a href="/technical-skills" className="nav-link">Skills</a>
-                <a href="/contact" className="nav-link">Contact</a>
+                <Link to="/" className="nav-link" activeClassName="active">Home</Link>
+                <Link to="/about" className="nav-link" activeClassName="active">About</Link>
+                <Link to="/technical-skills" className="nav-link" activeClassName="active">Skills</Link>
+                <Link to="/contact" className="nav-link" activeClassName="active">Contact</Link>
             </nav>
         </header>
     );

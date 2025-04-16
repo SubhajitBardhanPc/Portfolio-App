cde3b8b454d952bfa9394b649a61e55c4dcd4d32
bardhansubhajit3@gmail.com <bardhansubhajit3@gmail.com>
2025-04-16 21:06:43 +0530

    done

--- My Portfolio/client/src/pages/Contact.jsx
+++ My Portfolio/client/src/pages/Contact.jsx
@@ -9,9 +9,13 @@ const Contact = () => {
         <a href="tel:+919433601566" className="contact-card phone-card">
           <h3>Phone</h3>
           <p>+91-9433601566</p>
+          <br></br>
+          <p>+91-6289970438</p>
         </a>
         <a href="https://linkedin.com/in/subhajitbardhan" target="_blank" rel="noopener noreferrer" className="contact-card linkedin-card">
           <h3>LinkedIn</h3>
+
+          <br />
           <p>View Profile</p>
         </a>
       </div>
@@ -20,10 +24,12 @@ const Contact = () => {
       <div className="row">
         <a href="https://github.com/subhajit00007903" target="_blank" rel="noopener noreferrer" className="contact-card github-card">
           <h3>GitHub Profile 1</h3>
+          <br />
           <p>Visit GitHub (Profile 1)</p>
         </a>
         <a href="https://github.com/SubhajitBardhanPc" target="_blank" rel="noopener noreferrer" className="contact-card github-card">
           <h3>GitHub Profile 2</h3>
+          <br />
           <p>Visit GitHub (Profile 2)</p>
         </a>
       </div>
@@ -31,7 +37,8 @@ const Contact = () => {
       {/* Email Card */}
       <a href="mailto:subhajit.bardhan.csbs25@heritageit.edu.in" className="contact-card email-card email-center">
         <h3>Email</h3>
-        <p>subhajit.bardhan.csbs25@heritageit.edu.in</p>
+        <br />
+        <p>subhajitbardhan1@gmail.com</p>
       </a>
     </div>
   );

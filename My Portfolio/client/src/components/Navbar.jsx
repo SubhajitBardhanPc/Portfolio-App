cf3f696654a5e312cad8d0739f91f501ec0206e2
bardhansubhajit3@gmail.com <bardhansubhajit3@gmail.com>
2025-04-16 17:21:57 +0530

    Header Logo Done

--- My Portfolio/client/src/components/Navbar.jsx
+++ My Portfolio/client/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import "./Navbar.css"; // Importing CSS file
-
+import logo from "../assets/logo.png"; // Importing logo image
 const Navbar = () => {
     
 
     return (
         <header className="header">
-            <div className="logo">MyBrand</div>
+            <div className="logo">
+              {/* Add the developer logo here */}
+              <img src={logo}  className="developer-logo" />
+                
+            </div>
            
             <nav className="navbar">
                 <a href="#" className="nav-link">Home</a>
